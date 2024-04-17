fe44f0551ad071abc964a4e1f4f9e165482a10ca
Mihai Bîrsan <alexandru.m.birsan@gmail.com>
2024-04-17 07:05:32 +0000

    Features specific to parsing Lidl receipts

--- txt2csv.mjs
+++ txt2csv.mjs
@@ -5,15 +5,26 @@ import csvWriter from 'csv-writer';
 const inputFile = process.argv[2];
 const outputFile = inputFile.replace('.txt', '.csv');
 
+// Example: data/lidl/2024.02.17_2600017983202402177163.jpg.csv
+const discountPattern = /^DISCOUNT (\d+,\d{2})-(A|B|C|D)$/;
+// Example: data/lidl/2024.02.20_26000157842024022023795.jpg.txt
+const followOnPattern = /^(\d+,\d{2}) ?(A|B|C|D)$/;
 const startPattern = /^Lei$/;
 const endPattern = /^SUBTOTAL /;
-const dataPattern = /^(.+) (\d+,\d{2}) (A|B|C|D)$/;
+// Example: data/lidl/2024.02.09_26000343862024020923426.jpg.csv:7
+const dataPattern = /^(.+) (\d+,\d{2}) ?(A|B|C|D)$/;
 
 const readStream = fs.createReadStream(inputFile);
 const writeStream = csvWriter.createArrayCsvWriter({
     path: outputFile,
     header: ['Produs', 'Preț']  // replace with your CSV headers
 });
+
+function stringSafeAddition(a, b) {
+    const aFloat = Number.parseFloat(a.replace(',', '.'));
+    const bFloat = Number.parseFloat(b.replace(',', '.'));
+    return (aFloat + bFloat).toLocaleString('en-US', {minimumFractionDigits: 2});
+}
 
 const rl = readline.createInterface({
     input: readStream,
@@ -30,9 +41,10 @@ rl.on('line', (line) => {
     }
 
     if (startPatternMatched && line.match(endPattern)) {
-        const subtotalData = line.match(/\d+,\d{2}$/);
+        // Example: data/lidl/2024.02.26_26000157822024022646732.jpg.csv:10
+        const subtotalData = line.match(/\d+, ?\d{2}$/);
         if (subtotalData) {
-            const subtotal = Number.parseInt(subtotalData[0].replace(',', ''));
+            const subtotal = Number.parseInt(subtotalData[0].replaceAll(/[, ]/g, ''));
             const checksum = records.reduce(
                 (acc, record) => acc + Number.parseFloat(record[1].replace('.', '')),
                 0
@@ -54,6 +66,16 @@ rl.on('line', (line) => {
         if (data) {
             const price = data[2].replace(',', '.');
             records.push([data[1], price]);
+        }
+        const followOn = line.match(followOnPattern);
+        if (followOn) {
+            const followOnRecord = records.at(-1);
+            followOnRecord[1] = stringSafeAddition(followOnRecord[1], followOn[1]);
+        }
+        const discount = line.match(discountPattern);
+        if (discount) {
+            const discountedRecord = records.at(-1);
+            discountedRecord[1] = stringSafeAddition(discountedRecord[1], `-${discount[1]}`);
         }
     }
 });
